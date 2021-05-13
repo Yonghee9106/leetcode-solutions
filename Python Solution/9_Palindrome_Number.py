612999e0025fdd6723f75fd3d44ca776c5469f01
Yonghee <79752787+Yonghee9106@users.noreply.github.com>
2021-05-13 22:47:42 +1000

    Add files via upload

---
+++ Python Solution/9_Palindrome_Number.py
@@ -0,0 +1,26 @@
+##################
+# First Solution #
+##################
+
+# class Solution:
+#     def isPalindrome(self, x):
+#         for_num = x
+#         rev_num = 0
+#
+#         while x > 0:                                # if x is negative number, it's not palindrome
+#             rev_num = (rev_num * 10) + (x % 10)     # making reversed number
+#             x = x // 10
+#
+#         return for_num == rev_num
+
+
+###################
+# Second Solution #
+###################
+
+class Solution:
+    def isPalindrome(self, x):                      # [start:stop:step], -1 step means reverse step
+        return str(x)[::-1] == str(x)               # only sequence data types can be sliced in this way
+
+
+print(Solution().isPalindrome(-121))
