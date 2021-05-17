c697f49c3493bc2aa7572647875319eea53363b0
Yonghee <79752787+Yonghee9106@users.noreply.github.com>
2021-05-17 19:31:35 +1000

    Update 14_Longest_Common_Prefix.py

--- Python Solution/14_Longest_Common_Prefix.py
+++ Python Solution/14_Longest_Common_Prefix.py
@@ -25,7 +25,24 @@ class Solution(object):
 # Second Solution #
 ###################
 
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if not strs:
+            return ""
 
+        min_str = min(strs)
+        max_str = max(strs)
+
+        for index, char in enumerate(min_str):
+            if char != max_str[index]:
+                return min_str[:index]
+        return min_str
+    
+    
 
 ###################
 # Third Solution #
