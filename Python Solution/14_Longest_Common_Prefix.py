d46aa14e76a40fbda28330ad917e4e1397168172
Yonghee <79752787+Yonghee9106@users.noreply.github.com>
2021-05-18 15:44:01 +1000

    Update 14_Longest_Common_Prefix.py

--- Python Solution/14_Longest_Common_Prefix.py
+++ Python Solution/14_Longest_Common_Prefix.py
@@ -2,22 +2,22 @@
 # First Solution #
 ##################
 
-class Solution(object):
-    def longestCommonPrefix(self, strs):
-        """
-        :type strs: List[str]
-        :rtype: str
-        """
-        if not strs:                                # if strs list is empty,
-            return ""
-
-        shortest = min(strs, key=len)               # assign shortest word in strs by length
-
-        for index, char in enumerate(shortest):
-            for others in strs:
-                if others[index] != char:           # if any character from left is different,
-                    return shortest[:index]         # return before that index
-        return shortest                             # if there is no difference between the shortest and other words in strs, return min
+# class Solution(object):
+#     def longestCommonPrefix(self, strs):
+#         """
+#         :type strs: List[str]
+#         :rtype: str
+#         """
+#         if not strs:                                # if strs list is empty,
+#             return ""
+# 
+#         shortest = min(strs, key=len)               # assign shortest word in strs by length
+# 
+#         for index, char in enumerate(shortest):
+#             for others in strs:
+#                 if others[index] != char:           # if any character from left is different,
+#                     return shortest[:index]         # return before that index
+#         return shortest                             # if there is no difference between the shortest and other words in strs, return min
 
 
 
