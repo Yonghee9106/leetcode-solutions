309169f51046d2d12faa5889a8deef8c76aaef55
Yonghee <79752787+Yonghee9106@users.noreply.github.com>
2021-05-26 19:32:44 +1000

    Create 0014_Longest_Common_Prefix.py

---
+++ Python/0014_Longest_Common_Prefix.py
@@ -0,0 +1,46 @@
+##################
+# First Solution #
+##################
+
+# class Solution(object):
+#     def longestCommonPrefix(self, strs):
+#         """
+#         :type strs: List[str]
+#         :rtype: str
+#         """
+#         if not strs:                                # if strs list is empty,
+#             return ""
+# 
+#         shortest = min(strs, key=len)               # assign shortest word in strs by length
+# 
+#         for index, char in enumerate(shortest):
+#             for others in strs:
+#                 if others[index] != char:           # if any character from left is different,
+#                     return shortest[:index]         # return before that index
+#         return shortest                             # if there is no difference between the shortest and other words in strs, return min
+
+
+
+###################
+# Second Solution #
+###################
+
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if not strs:                                # if strs list is empty,
+            return ""
+
+        min_str = min(strs)                         # returns min based on alphabetical order
+        max_str = max(strs)                         # returns max based on alphabetical order
+
+        for index, char in enumerate(min_str):
+            if char != max_str[index]:              # only compare min and max words
+                return min_str[:index]              # because they're the words that make the most difference
+        return min_str                              # if there is no difference between min and max, return min
+
+
+print(Solution().longestCommonPrefix(["flower", "flow", "flight"]))
