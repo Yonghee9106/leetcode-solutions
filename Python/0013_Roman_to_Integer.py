0dbf19ef8a2c90267243385e55e3011757d663b1
Yonghee <79752787+Yonghee9106@users.noreply.github.com>
2021-05-29 18:57:13 +1000

    Update 0013_Roman_to_Integer.py

--- Python/0013_Roman_to_Integer.py
+++ Python/0013_Roman_to_Integer.py
@@ -8,7 +8,7 @@
 #         num = 0
 #
 #         for i in range(0, len(s) - 1):                  # range from 0 to len(s)-1, because there is no s[i+1] at the end of s
-#             if Roman_Dic[s[i]] < Roman_Dic[s[i+1]]:     # if right Roman is bigger than left, that is the expression of 4
+#             if Roman_Dic[s[i]] < Roman_Dic[s[i+1]]:     # if right Roman is bigger than left, that is the expression of 4 and 9
 #                 num -= Roman_Dic[s[i]]                  # So, subtract it
 #             else:                                       # if right Roman is smaller than left, that is normal
 #                 num += Roman_Dic[s[i]]                  # So, just add it
@@ -25,7 +25,7 @@ class Solution:
         Roman_Dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
         num = 0
 
-        s = s.replace("IV", "IIII").replace("IX", "VIIII")          # replace exceptions
+        s = s.replace("IV", "IIII").replace("IX", "VIIII")          # replace the exceptions
         s = s.replace("XL", "XXXX").replace("XC", "LXXXX")
         s = s.replace("CD", "CCCC").replace("CM", "DCCCC")
 
