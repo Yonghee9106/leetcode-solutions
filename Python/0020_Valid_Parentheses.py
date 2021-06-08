43ab2960e5a63e77028e55389daf3ba2714c1b82
Yonghee <79752787+Yonghee9106@users.noreply.github.com>
2021-06-08 17:49:02 +1000

    Update 0020_Valid_Parentheses.py

--- Python/0020_Valid_Parentheses.py
+++ Python/0020_Valid_Parentheses.py
@@ -1,1 +1,18 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        dic = {')': '(', '}': '{', ']': '['}
+        dic_open = dic.values()
 
+        for ch in s:
+            if ch in dic_open:
+                stack.append(ch)
+            else:
+                if len(stack) > 0 and stack[-1] == dic[ch]:
+                    stack.pop()
+                else:
+                    return False
+        return len(stack) == 0
+
+
+print(Solution().isValid("()[]{}"))
