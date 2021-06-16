e0aa918094c61a13819d0325a0c0157fabaec727
Yonghee <79752787+Yonghee9106@users.noreply.github.com>
2021-06-16 23:32:02 +1000

    Update 0020_Valid_Parentheses.py

--- Python/0020_Valid_Parentheses.py
+++ Python/0020_Valid_Parentheses.py
@@ -7,8 +7,8 @@ class Solution:
         for ch in s:
             if ch in dic_open:                                  # if ch is open bracket
                 stack.append(ch)                                # add it in the list
-            else:
-                if len(stack) > 0 and stack[-1] == dic[ch]:     # if the closed bracket is paired with the last bracket of the stack
+            else:                                               # if ch is closed bracket
+                if len(stack) > 0 and stack[-1] == dic[ch]:     # and if the closed bracket is paired with the last bracket of the stack
                     stack.pop()                                 # pop it
                 else:                                           # if not paired,
                     return False                                # it is not valid parentheses
