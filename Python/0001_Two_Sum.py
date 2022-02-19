7fbf9709bcd5b0d8b81c80bb2b56f8bd4e065af0
Yonghee <79752787+Yonghee9106@users.noreply.github.com>
2022-02-20 00:16:08 +1100

    Update 0001_Two_Sum.py

--- Python/0001_Two_Sum.py
+++ Python/0001_Two_Sum.py
@@ -4,9 +4,11 @@ class Solution:
         
         for index, num in enumerate(nums):      # enumerate() returns index and value(num)
             diff = target - num
+            
             if diff in diff_dic:                # if there is a diff in diff_dic
                 return [diff_dic[diff], index]  # diff's index and current index is the two sum combination
             diff_dic[num] = index               # if there isn't a diff, add current num as key and index as value
+            
         return print("No two sum solution!")
 
 print(Solution().twoSum([2, 7, 11, 15], 9))
