ab5f610f69214823d54768ad815833bc43197d42
Yonghee <79752787+Yonghee9106@users.noreply.github.com>
2022-02-19 20:25:19 +1100

    Add files via upload

---
+++ 0001_Two_Sum.js
@@ -0,0 +1,16 @@
+var twoSum = function(nums, target) {
+    
+    let diff_dic = {};
+
+    for(let i=0; i<nums.length; i++) {
+        diff = target - nums[i];
+
+        if(diff_dic[diff] != null) {
+            return [diff_dic[diff], i];
+        }
+
+        diff_dic[nums[i]] = i;
+    }
+
+    return [];
+};
