8009322fcac57c0f62772b8b2fd693f9cd8bef17
Yonghee <79752787+Yonghee9106@users.noreply.github.com>
2022-02-19 20:40:06 +1100

    Update 0001_Two_Sum.js

--- JavaScript/0001_Two_Sum.js
+++ JavaScript/0001_Two_Sum.js
@@ -5,17 +5,17 @@
  */
 
 var twoSum = function(nums, target) {
-    
-    let diff_dic = {};
+
+    let diff_dic = {};                      // key: difference, value: index
 
     for(let i=0; i<nums.length; i++) {
         diff = target - nums[i];
 
-        if(diff_dic[diff] != null) {
-            return [diff_dic[diff], i];
+        if(diff_dic[diff] != null) {        // if there is a diff in diff_dic
+            return [diff_dic[diff], i];     // diff's index and current index are the two sum combination
         }
 
-        diff_dic[nums[i]] = i;
+        diff_dic[nums[i]] = i;              // if there isn't a diff, add num[i] as a key and index as a value
     }
 
     return [];
