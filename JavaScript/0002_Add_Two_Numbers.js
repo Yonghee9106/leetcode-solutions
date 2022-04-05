c93388c18354912e484bf2dd88f433bf7f5f8195
Yonghee <79752787+Yonghee9106@users.noreply.github.com>
2022-04-05 13:56:05 +1000

    Update 0002_Add_Two_Numbers.js

--- JavaScript/0002_Add_Two_Numbers.js
+++ JavaScript/0002_Add_Two_Numbers.js
@@ -11,29 +11,29 @@
  * @return {ListNode}
  */
 var addTwoNumbers = function(l1, l2) {
-    let summedList = new ListNode(0, null);
-    let head = summedList;
+    let summedList = new ListNode(0, null);                 // new linked list to return the sum
+    let head = summedList;                                  // copy linked list but same reference
     let sum = 0;
     let carry = 0;
 
-    while(l1 !== null || l2 !== null || sum > 0) {
-        if(l1 !== null) {
-            sum += l1.val;
-            l1 = l1.next;
+    while(l1 !== null || l2 !== null || sum > 0) {          // if input linked-lists are not at the tails and sum is bigger than 0
+        if(l1 !== null) {                                   // if input linked list l1 is not at the tail
+            sum += l1.val;                                  // add to sum
+            l1 = l1.next;                                   // and go to next node
         }
-        if(l2 !== null) {
-            sum += l2.val;
-            l2 = l2.next;
+        if(l2 !== null) {                                   // if input linked list l2 is not at the tail
+            sum += l2.val;                                  // add to sum
+            l2 = l2.next;                                   // and go to next node
         }
-        if(sum >= 10) {
-            carry = 1;
-            sum -= 10;
+        if(sum >= 10) {                                     // if sum is bigger than 10
+            carry = 1;                                      // create carry
+            sum -= 10;                                      // and leave last digit only
         }
-        head.next = new ListNode(sum, null);
-        head = head.next;
-
+        head.next = new ListNode(sum, null);                // create new node with last digit and connect to head
+        head = head.next;                                   // to repeat this process, head should be head.next
+                                                            // otherwise it will overwrite second node
         sum = carry;
         carry = 0;
     }
-    return summedList.next;
+    return summedList.next;                                 // return next node of summedList because it starts with 0
 };
