0e904cde7348c9aaba62af843308ef942600a886
Yonghee <79752787+Yonghee9106@users.noreply.github.com>
2022-04-12 14:28:40 +1000

    Update 0002_Add_Two_Numbers.java

--- Java/0002_Add_Two_Numbers.java
+++ Java/0002_Add_Two_Numbers.java
@@ -10,30 +10,30 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode summedList = new ListNode(0, null);
-        ListNode head = summedList;
+        ListNode summedList = new ListNode(0, null);            // create new linked for return
+        ListNode head = summedList;                             // copy linked list but same reference
         int carry = 0;
         int sum = 0;
 
-        while(l1!=null || l2!=null || sum>0) {
-            if(l1 != null) {
-                sum += l1.val;
-                l1 = l1.next;
+        while(l1!=null || l2!=null || sum>0) {                  // if input linked-lists are not at the tails or sum is bigger than 0
+            if(l1 != null) {                                    // if input linked list l1 is not at the tail
+                sum += l1.val;                                  // add to sum
+                l1 = l1.next;                                   // and go to next node
             }
-            if(l2 != null) {
-                sum += l2.val;
-                l2 = l2.next;
+            if(l2 != null) {                                    // if input linked list l2 is not at the tail
+                sum += l2.val;                                  // add to sum
+                l2 = l2.next;                                   // and go to next node
             }
-            if(sum >= 10) {
-                carry = 1;
-                sum -= 10;
+            if(sum >= 10) {                                     // if sum is bigger than 10
+                carry = sum / 10;                               // create carry
+                sum = sum % 10;                                 // and leave last digit only
             }
-            head.next = new ListNode(sum, null);
-            head = head.next;
-
+            head.next = new ListNode(sum, null);                // create new node with last digit and connect to head
+            head = head.next;                                   // to repeat this process, head should be head.next
+                                                                // otherwise it will overwrite second node
             sum = carry;
             carry = 0;
         }
-        return summedList.next;
+        return summedList.next;                                 // return next node of summedList because it starts with 0
     }
 }
