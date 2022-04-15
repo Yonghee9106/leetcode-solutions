2c2383c767519355c4f19bb024c602e1765db640
Yonghee <79752787+Yonghee9106@users.noreply.github.com>
2022-04-16 02:47:33 +1000

    Update 0003_Longest_Substring_Without_Repeating_Characters.java

--- Java/0003_Longest_Substring_Without_Repeating_Characters.java
+++ Java/0003_Longest_Substring_Without_Repeating_Characters.java
@@ -2,18 +2,18 @@ class Solution {
     public int lengthOfLongestSubstring(String s) {
         int startIndex = 0;                                             // first index of substring
         int endIndex = 0;                                               // last index of substring
-        int maxLength = 0;
+        int maxLength = 0;                                              // max length of substring
 
-        Set<Character> substring = new HashSet<>();
+        Set<Character> substring = new HashSet<>();                     // hashset can't contain duplicate values
 
-        while(endIndex < s.length()) {
-            if(!substring.contains(s.charAt(endIndex))) {
-                substring.add(s.charAt(endIndex));
+        while(endIndex < s.length()) {                                  // while end index is less than input string s
+            if(!substring.contains(s.charAt(endIndex))) {               // if substring doesn't contains duplicate value
+                substring.add(s.charAt(endIndex));                      // add that character to substring
                 endIndex++;
-                maxLength = Math.max(maxLength, substring.size());
-            } else {
-                substring.remove(s.charAt(startIndex));
-                startIndex++;
+                maxLength = Math.max(maxLength, substring.size());      // find max
+            } else {                                                    // if substring contains duplicate value
+                substring.remove(s.charAt(startIndex));                 // remove first start index character
+                startIndex++;                                           // so that we can search other substrings
             }
         }
         return maxLength;
