1fc015695c6479e98d37f2ef2e210de7a9fb54fe
Yonghee <79752787+Yonghee9106@users.noreply.github.com>
2022-04-19 15:02:45 +1000

    Update 0003_Longest_Substring_Without_Repeating_Characters.js

--- JavaScript/0003_Longest_Substring_Without_Repeating_Characters.js
+++ JavaScript/0003_Longest_Substring_Without_Repeating_Characters.js
@@ -3,22 +3,22 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-    if(s.length == 0) {
-        return 0;
+    if(s.length == 0) {                                     // if size of input is 0,
+        return 0;                                           // return 0, no substring
     }
 
-    const substring = new Map();
+    const substring = new Map();                            // key: character, value: index
 
     var startIndex = 0;
     var maxLen = 0;
     
-    for(let i=0; i<s.length; i++) {
-        if(substring.has(s[i])) {
-            startIndex = Math.max(substring.get(s[i])+1, startIndex);
+    for(let i=0; i<s.length; i++) {                         // for all input s characters
+        if(substring.has(s[i])) {                           // if current character is existing already,
+            startIndex = Math.max(substring.get(s[i])+1, startIndex);   // move start index forward, can't go backward
         }
 
-        substring.set(s[i], i);
-        maxLen = Math.max(i-startIndex+1, maxLen);
+        substring.set(s[i], i);                             // add current character and index
+        maxLen = Math.max(i-startIndex+1, maxLen);          // get max length of substring
     }
     return maxLen;
 };
