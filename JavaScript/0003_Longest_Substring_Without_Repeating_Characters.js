2d91f95a890ca340d7928a9fe0ba51dd7e813978
Yonghee <79752787+Yonghee9106@users.noreply.github.com>
2022-04-19 14:44:40 +1000

    Update 0003_Longest_Substring_Without_Repeating_Characters.js

--- JavaScript/0003_Longest_Substring_Without_Repeating_Characters.js
+++ JavaScript/0003_Longest_Substring_Without_Repeating_Characters.js
@@ -2,6 +2,19 @@
  * @param {string} s
  * @return {number}
  */
- var lengthOfLongestSubstring = function(s) {
+var lengthOfLongestSubstring = function(s) {
+    const seen = new Map();
+
+    var start = 0;
+    var max = 0;
     
+    for(let i=0; i<s.length; i++) {
+        if(seen.has(s[i])) {
+            start = Math.max(seen.get(s[i])+1, start);
+        }
+
+        seen.set(s[i], i);
+        max = Math.max(i-start+1, max);
+    }
+    return max;
 };
