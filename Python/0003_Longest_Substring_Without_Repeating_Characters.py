a02bd57b0148cfca5ba291ca942ecaf9a9f3bbd7
Yonghee <79752787+Yonghee9106@users.noreply.github.com>
2022-04-21 16:02:45 +1000

    Update 0003_Longest_Substring_Without_Repeating_Characters.py

--- Python/0003_Longest_Substring_Without_Repeating_Characters.py
+++ Python/0003_Longest_Substring_Without_Repeating_Characters.py
@@ -8,9 +8,9 @@ class Solution:
             return 0                                                    # return 0, no substring
         
         for i in range(len(s)):                                         # for all input s characters
-            # if there's duplicated character, move start index next to duplicated character index
+            # if there's duplicated character, move start index next to duplicated character's index
             if s[i] in substring and startIndex <= substring[s[i]]:     # if current character is existing in substring, and current index is bigger than startIndex
-                startIndex = substring[s[i]] + 1                        # move start index forward, can't go backward, substring[s[i]] == duplicated character index
+                startIndex = substring[s[i]] + 1                        # move startIndex forward, can't go backward, substring[s[i]] == duplicated character's index
             else:
                 maxLength = max(maxLength, i - startIndex + 1)          # get max length of substring, i-startIndex+1 == length of current substring
 
