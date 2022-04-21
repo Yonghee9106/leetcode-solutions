229e2b84f399aee10d3a2c310e700eb9dde9494d
Yonghee <79752787+Yonghee9106@users.noreply.github.com>
2022-04-21 15:58:23 +1000

    Update 0003_Longest_Substring_Without_Repeating_Characters.py

--- Python/0003_Longest_Substring_Without_Repeating_Characters.py
+++ Python/0003_Longest_Substring_Without_Repeating_Characters.py
@@ -2,19 +2,22 @@ class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         startIndex = 0                                                  # start index of substring
         maxLength = 0                                                   # max length of substring
-        substring = {}
+        substring = {}                                                  # dictionary {key: character, value: index}
 
         if len(s) == 0:                                                 # if size of input s is 0,
             return 0                                                    # return 0, no substring
         
-        for i in range(len(s)):
-            if s[i] in substring and startIndex <= substring[s[i]]:
-                startIndex = substring[s[i]] + 1
+        for i in range(len(s)):                                         # for all input s characters
+            # if there's duplicated character, move start index next to duplicated character index
+            if s[i] in substring and startIndex <= substring[s[i]]:     # if current character is existing in substring, and current index is bigger than startIndex
+                startIndex = substring[s[i]] + 1                        # move start index forward, can't go backward, substring[s[i]] == duplicated character index
             else:
-                maxLength = max(maxLength, i - startIndex + 1)
+                maxLength = max(maxLength, i - startIndex + 1)          # get max length of substring, i-startIndex+1 == length of current substring
 
-            substring[s[i]] = i
+            substring[s[i]] = i                                         # add current character and index to substring
 
         return maxLength
 
-print(Solution().lengthOfLongestSubstring("abcabcbb"))
+print(Solution().lengthOfLongestSubstring("abcabcbb"))                  # Example 1
+print(Solution().lengthOfLongestSubstring("bbbbb"))                     # Example 2
+print(Solution().lengthOfLongestSubstring("pwwkew"))                    # Example 3
