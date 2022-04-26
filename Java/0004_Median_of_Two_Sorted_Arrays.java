83cca3c29c4b7aa1513b026f9b2eeb2dbf6c3b90
Yonghee <79752787+Yonghee9106@users.noreply.github.com>
2022-04-26 22:39:50 +1000

    Update 0004_Median_of_Two_Sorted_Arrays.java

--- Java/0004_Median_of_Two_Sorted_Arrays.java
+++ Java/0004_Median_of_Two_Sorted_Arrays.java
@@ -1,8 +1,18 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int nums1Size = nums2.length;
+        int nums2Size = nums2.length;
+        if(nums1Size < nums2Size) {
+            return findMedianSortedArrays(nums2, nums1);
+        }
+
         
     }
 }
+
+// Hint
+// https://ganeshpr227.medium.com/logarithmic-algorithm-for-finding-median-of-two-sorted-arrays-of-different-sizes-aaecf302057e
+// https://www.youtube.com/watch?v=LPFhl65R7ww
 
 // Hints
 // https://ganeshpr227.medium.com/logarithmic-algorithm-for-finding-median-of-two-sorted-arrays-of-different-sizes-aaecf302057e
