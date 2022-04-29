700e516a4f906e9ff19bb8108de17dc0571fcaac
Yonghee <79752787+Yonghee9106@users.noreply.github.com>
2022-04-30 09:24:56 +1000

    Update 0004_Median_of_Two_Sorted_Arrays.java

--- Java/0004_Median_of_Two_Sorted_Arrays.java
+++ Java/0004_Median_of_Two_Sorted_Arrays.java
@@ -1,12 +1,5 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        if(nums1 == null && nums2 == null) {
-            return 0;
-        }
-
-        if(nums1 == null) {
-            int n = nums2.length;
-            return nums2[(n - 1) / 2] * 0.5 + nums2[n / 2] * 0.5;
-        }
+        
     }
 }
