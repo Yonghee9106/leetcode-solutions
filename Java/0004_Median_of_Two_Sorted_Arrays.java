497d51d755188caeeebdc024d042fc945beffa8e
Yonghee <79752787+Yonghee9106@users.noreply.github.com>
2022-05-02 23:59:47 +1000

    Update 0004_Median_of_Two_Sorted_Arrays.java

--- Java/0004_Median_of_Two_Sorted_Arrays.java
+++ Java/0004_Median_of_Two_Sorted_Arrays.java
@@ -3,5 +3,17 @@ class Solution {
         int nums1Len = nums1.length;
         int nums2Len = nums2.length;
         int sumLen = nums1Len + nums2Len;
+
+        if(sumLen % 2 != 0) {
+            return findKthSmallest(nums1, nums1Len, 0, nums2, nums2Len, 0, sumLen / 2 + 1);
+        }
+        else {
+            return (findKthSmallest(nums1, nums1Len, 0, nums2, nums2Len, 0, sumLen / 2)
+                    + findKthSmallest(nums1, nums1Len, 0, nums2, nums2Len, 0, sumLen / 2 + 1)) / 2.0;
+        }
+    }
+
+    public static int findKthSmallest(int[] a, int m, int begin1, int[] b, int n, int begin2, int k) {
+
     }
 }
