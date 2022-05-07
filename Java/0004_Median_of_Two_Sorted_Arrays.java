516fe054dc9005977d75bc397a01b3eccb1d6fb9
Yonghee <79752787+Yonghee9106@users.noreply.github.com>
2022-05-07 21:30:33 +1000

    Update 0004_Median_of_Two_Sorted_Arrays.java

--- Java/0004_Median_of_Two_Sorted_Arrays.java
+++ Java/0004_Median_of_Two_Sorted_Arrays.java
@@ -1,34 +1,32 @@
 class Solution {
-    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int nums1Len = nums1.length;
-        int nums2Len = nums2.length;
-        int sumLen = nums1Len + nums2Len;
+    public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int nums1len = nums1.length;
+        int nums2len = nums2.length;
+        int sumLen = nums1len + nums2len;
 
-        if(sumLen % 2 != 0) {
-            return findKthSmallest(nums1, nums1Len, 0, nums2, nums2Len, 0, sumLen / 2 + 1);
-        }
-        else {
-            return (findKthSmallest(nums1, nums1Len, 0, nums2, nums2Len, 0, sumLen / 2)
-                    + findKthSmallest(nums1, nums1Len, 0, nums2, nums2Len, 0, sumLen / 2 + 1)) / 2.0;
+        if (sumLen % 2 != 0) {
+            return findKthSmallest(nums1, nums1len, 0, nums2, nums2len, 0, sumLen / 2 + 1);
+        } else {
+            return (findKthSmallest(nums1, nums1len, 0, nums2, nums2len, 0, sumLen / 2)
+                    + findKthSmallest(nums1, nums1len, 0, nums2, nums2len, 0, sumLen / 2 + 1)) / 2.0;
         }
     }
-
+    
     public static int findKthSmallest(int[] nums1, int len1, int begin1, int[] nums2, int len2, int begin2, int kth) {
-        if(len1 > len2)
+        if (len1 > len2)
             return findKthSmallest(nums2, len2, begin2, nums1, len1, begin1, kth);
-        if(len1 == 0)
+        if (len1 == 0)
             return nums2[begin2 + kth - 1];
-        if(kth == 1)
+        if (kth == 1)
             return Integer.min(nums1[begin1], nums2[begin2]);
 
-        int nums1Part = Integer.min(kth/2, len1);
-        int nums2Part = kth = nums1Part;
-
-        if(nums1[begin1 + nums1Part - 1] == nums2[begin2 + nums2Part - 1])
-            return nums1[begin1 + nums1Part - 1];
-        else if(nums1[begin1 + nums1Part - 1] > nums2[begin2 + nums2Part - 1])
-            return findKthSmallest(nums1, nums1Part, begin1, nums2, len2 - nums2Part, begin2 + nums2Part, kth - nums2Part);
+        int nums1part = Integer.min(kth / 2, len1);
+        int nums2part = kth - nums1part;
+        if (nums1[begin1 + nums1part - 1] == nums2[begin2 + nums2part - 1])
+            return nums1[begin1 + nums1part - 1];
+        else if (nums1[begin1 + nums1part - 1] > nums2[begin2 + nums2part - 1])
+            return findKthSmallest(nums1, len1, begin1, nums2, len2 - nums2part, begin2 + nums2part, kth - nums2part);
         else
-            return findKthSmallest(nums1, len1 - nums1Part, begin1 + nums1Part, nums2, nums2Part, begin2, kth - nums1Part);
+            return findKthSmallest(nums1, len1 - nums1part, begin1 + nums1part, nums2, len2, begin2, kth - nums1part);
     }
 }
