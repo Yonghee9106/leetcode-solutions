0e79b594d6224a90f4622af45beb414522030974
Yonghee <79752787+Yonghee9106@users.noreply.github.com>
2022-05-11 21:10:50 +1000

    Update 0004_Median_of_Two_Sorted_Arrays.java

--- Java/0004_Median_of_Two_Sorted_Arrays.java
+++ Java/0004_Median_of_Two_Sorted_Arrays.java
@@ -42,3 +42,4 @@ class Solution {
 
 // References
 // https://www.youtube.com/watch?v=LPFhl65R7ww
+// https://github.com/mission-peace/interview/blob/master/src/com/interview/binarysearch/MedianOfTwoSortedArrayOfDifferentLength.java
