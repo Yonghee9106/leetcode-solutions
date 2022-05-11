9765f444a0d9b3e700486d6bb92e705b0f267707
Yonghee <79752787+Yonghee9106@users.noreply.github.com>
2022-05-11 21:20:38 +1000

    Update 0004_Median_of_Two_Sorted_Arrays.java

--- Java/0004_Median_of_Two_Sorted_Arrays.java
+++ Java/0004_Median_of_Two_Sorted_Arrays.java
@@ -12,6 +12,8 @@ class Solution {
         int nums1High = nums1Len;
         // find the partition where all left side numbers of nums1 and nums2 are smaller than right side numbers
         while (nums1Low <= nums1High) {
+            // if the total length of two arrays is even, the number of left side numbers == the number of right side numbers
+            // if the total length of two arrays is odd, the number of left sisde numbers + 1 == the number of right side numbers
             int nums1Partition = (nums1Low + nums1High)/2;
             int nums2Partition = (nums1Len + nums2Len + 1)/2 - nums1Partition;
 
