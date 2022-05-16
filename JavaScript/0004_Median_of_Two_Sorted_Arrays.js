06b91dd57b3b5ce6e9ad029e96cecb17d9591ab0
Yonghee <79752787+Yonghee9106@users.noreply.github.com>
2022-05-16 22:30:47 +1000

    Update 0004_Median_of_Two_Sorted_Arrays.js

--- JavaScript/0004_Median_of_Two_Sorted_Arrays.js
+++ JavaScript/0004_Median_of_Two_Sorted_Arrays.js
@@ -4,5 +4,11 @@
  * @return {number}
  */
  var findMedianSortedArrays = function(nums1, nums2) {
-    if(nums1.length > nums2.length) return findMedianSortedArrays(nums2, nums1);
+    
+    if(nums1.length > nums2.length) {
+        return findMedianSortedArrays(nums2, nums1);
+    }
+
+    let nums1Len = nums1.length;
+    let nums2Len = nums2.length;
 };
