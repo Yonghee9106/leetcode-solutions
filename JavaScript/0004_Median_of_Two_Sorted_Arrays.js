c8fb52d4b8fcaf3aa2720fccb471b5c0b3e38fa7
Yonghee <79752787+Yonghee9106@users.noreply.github.com>
2022-05-21 22:02:21 +1000

    Update 0004_Median_of_Two_Sorted_Arrays.js

--- JavaScript/0004_Median_of_Two_Sorted_Arrays.js
+++ JavaScript/0004_Median_of_Two_Sorted_Arrays.js
@@ -18,7 +18,20 @@
         const nums1Partition = Math.floor((nums1Low+nums1High)/2);
         const nums2Partition = Math.floor((nums1Len+nums2Len+1)/2) - nums1Partition;
 
-        const nums1LeftMax = nums1Partition == 0 ? Number.NEGATIVE_INFINITY : nums1[nums1Partition - 1];
+        const nums1LeftMax = (nums1Partition == 0) ? Number.NEGATIVE_INFINITY : nums1[nums1Partition - 1];
         const nums1RightMin = (nums1Partition == nums1Len) ? Number.POSITIVE_INFINITY : nums1[nums1Partition];
+
+        const nums2LeftMax = (nums2Partition == 0) ? Number.NEGATIVE_INFINITY : nums2[nums2Partition - 1];
+        const nums2RightMin = (nums2Partition == nums2Len) ? Number.POSITIVE_INFINITY : nums2[nums2Partition];
+
+        if(nums1LeftMax <= nums2RightMin && nums2LeftMax <= nums1RightMin) {
+            const lowMax = Math.max(nums1LeftMax, nums2LeftMax)
+            if( (nums1Len + nums2Len) % 2 == 1)
+                return lowMax
+            return (lowMax + Math.min(nums1RightMin, nums2RightMin)) / 2
+        } else if(nums1LeftMax < nums2RightMin) {
+            nums1Low = nums1Partition + 1
+        } else 
+            nums1High = nums1Partition - 1
     }
 };
