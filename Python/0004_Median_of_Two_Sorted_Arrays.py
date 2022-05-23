3a96903ab30f264aa73be296ae1c31a3baa536e9
Yonghee <79752787+Yonghee9106@users.noreply.github.com>
2022-05-23 13:05:44 +1000

    Update 0004_Median_of_Two_Sorted_Arrays.py

--- Python/0004_Median_of_Two_Sorted_Arrays.py
+++ Python/0004_Median_of_Two_Sorted_Arrays.py
@@ -1,22 +1,23 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        sumLen=len(nums1)+len(nums2)
+        sumLen = len(nums1) + len(nums2)
 
         if(sumLen % 2 == 1):
-            return self.findKth(nums1,nums2,sumLen//2)
+            return self.findKth(nums1, nums2, sumLen//2)
         else:
-            return (self.findKth(nums1,nums2,sumLen//2-1)+self.findKth(nums1,nums2,sumLen//2))/2.0
+            return (self.findKth(nums1, nums2, sumLen//2-1) + self.findKth(nums1, nums2, sumLen//2)) / 2.0
             
-    def findKth(self,nums1,nums2,kth):
-        if len(nums1)>len(nums2):
-            nums1,nums2=nums2,nums1
+    def findKth(self, nums1, nums2, kth):
+        if len(nums1) > len(nums2):
+            nums1,nums2 = nums2,nums1
         if not nums1:
             return nums2[kth]
-        if kth==len(nums1)+len(nums2)-1:
-            return max(nums1[-1],nums2[-1])
-        i=len(nums1)//2
-        j=kth-i
-        if nums1[i]>nums2[j]:
-            return self.findKth(nums1[:i],nums2[j:],i)
+        if kth == len(nums1) + len(nums2) - 1:
+            return max(nums1[-1], nums2[-1])
+
+        i = len(nums1) // 2
+        j = kth - i
+        if nums1[i] > nums2[j]:
+            return self.findKth(nums1[:i], nums2[j:], i)
         else:
-            return self.findKth(nums1[i:],nums2[:j],j)
+            return self.findKth(nums1[i:], nums2[:j], j)
