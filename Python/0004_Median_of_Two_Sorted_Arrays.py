4779bc49d91700a8210c792c67e9e25f4e2ff6ce
Yonghee <79752787+Yonghee9106@users.noreply.github.com>
2022-05-23 13:47:29 +1000

    Update 0004_Median_of_Two_Sorted_Arrays.py

--- Python/0004_Median_of_Two_Sorted_Arrays.py
+++ Python/0004_Median_of_Two_Sorted_Arrays.py
@@ -7,12 +7,15 @@ class Solution:
         else:
             return (self.findKth(nums1, nums2, sumLen//2-1) + self.findKth(nums1, nums2, sumLen//2)) / 2.0
             
+    # find kth biggest element in two arrays
     def findKth(self, nums1, nums2, kth):
         # if length of nums1 is bigger than nums2, the nums2 partition can be out of bounds for nums2
         if len(nums1) > len(nums2):
             nums1, nums2 = nums2, nums1
+        # if nums1 is empty, find kth in nums2
         if not nums1:
             return nums2[kth]
+        # if kth is sume of two array lengths, return the biggest element
         if kth == len(nums1) + len(nums2) - 1:
             return max(nums1[-1], nums2[-1])
 
