b858ff3aa699515716cc067e904c458f627d1e59
Yonghee <79752787+Yonghee9106@users.noreply.github.com>
2022-05-23 12:16:41 +1000

    Update 0004_Median_of_Two_Sorted_Arrays.py

--- Python/0004_Median_of_Two_Sorted_Arrays.py
+++ Python/0004_Median_of_Two_Sorted_Arrays.py
@@ -1,3 +1,22 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        
+        sumLen=len(nums1)+len(nums2)
+
+        if(sumLen % 2 == 1):
+            return self.findKth(nums1,nums2,sumLen//2)
+        else:
+            return (self.findKth(nums1,nums2,sumLen//2-1)+self.findKth(nums1,nums2,sumLen//2))/2.0
+            
+    def findKth(self,nums1,nums2,k):
+        if len(nums1)>len(nums2):
+            nums1,nums2=nums2,nums1
+        if not nums1:
+            return nums2[k]
+        if k==len(nums1)+len(nums2)-1:
+            return max(nums1[-1],nums2[-1])
+        i=len(nums1)//2
+        j=k-i
+        if nums1[i]>nums2[j]:
+            return self.findKth(nums1[:i],nums2[j:],i)
+        else:
+            return self.findKth(nums1[i:],nums2[:j],j)
