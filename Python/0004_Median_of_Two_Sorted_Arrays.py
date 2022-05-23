fc03bd13c2e0783bcc8df47a92392f3e4bc4ae21
Yonghee <79752787+Yonghee9106@users.noreply.github.com>
2022-05-23 12:23:20 +1000

    Update 0004_Median_of_Two_Sorted_Arrays.py

--- Python/0004_Median_of_Two_Sorted_Arrays.py
+++ Python/0004_Median_of_Two_Sorted_Arrays.py
@@ -7,15 +7,15 @@ class Solution:
         else:
             return (self.findKth(nums1,nums2,sumLen//2-1)+self.findKth(nums1,nums2,sumLen//2))/2.0
             
-    def findKth(self,nums1,nums2,k):
+    def findKth(self,nums1,nums2,kth):
         if len(nums1)>len(nums2):
             nums1,nums2=nums2,nums1
         if not nums1:
-            return nums2[k]
-        if k==len(nums1)+len(nums2)-1:
+            return nums2[kth]
+        if kth==len(nums1)+len(nums2)-1:
             return max(nums1[-1],nums2[-1])
         i=len(nums1)//2
-        j=k-i
+        j=kth-i
         if nums1[i]>nums2[j]:
             return self.findKth(nums1[:i],nums2[j:],i)
         else:
