fd4d89020d77ddd5095bb1fc8c05699dcfe11455
Yonghee <79752787+Yonghee9106@users.noreply.github.com>
2022-05-28 17:48:16 +1000

    Update 0005_Longest_Palindromic_Substring.java

--- Java/0005_Longest_Palindromic_Substring.java
+++ Java/0005_Longest_Palindromic_Substring.java
@@ -1,7 +1,7 @@
 class Solution {
     public String longestPalindrome(String s) {
-        int startIndex;
-        int endIndex;
+        int startIndex = 0;
+        int endIndex = 0;
         int len = s.length();
 
         if(len < 2) {
@@ -9,12 +9,27 @@ class Solution {
         }
 
         for(int i=0; i<len; i++) {
-            expandCenter(s, i, i);
-            expandCenter(s, i, i+1);
+            int len1 = expandCenter(s, i, i);
+            int len2 = expandCenter(s, i, i+1);
+
+            if(endIndex < Math.max(len1, len2)) {
+                startIndex = (len1>len2) ? (i-len1/2) : (i-len2/2+1);
+                endIndex = Math.max(len1, len2);
+            }
         }
+        return s.substring(startIndex, endIndex+startIndex);
     }
 
     private int expandCenter(String s, int start, int end) {
-        
+        while(start >= 0 && end < s.length()) {
+            if(s.charAt(start) == s.charAt(end)) {
+                start--;
+                end++;
+            }
+            else {
+                break;
+            }
+        }
+        return end - start - 2 + 1;
     }
 }
