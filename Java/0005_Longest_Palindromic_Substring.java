0c3e63d4b2d2ace320755f3b9e8ea584b40edf53
Yonghee <79752787+Yonghee9106@users.noreply.github.com>
2022-05-28 14:06:41 +1000

    Update 0005_Longest_Palindromic_Substring.java

--- Java/0005_Longest_Palindromic_Substring.java
+++ Java/0005_Longest_Palindromic_Substring.java
@@ -1,5 +1,20 @@
 class Solution {
     public String longestPalindrome(String s) {
+        int startIndex;
+        int endIndex;
+        int len = s.length();
+
+        if(len < 2) {
+            return s;
+        }
+
+        for(int i=0; i<len; i++) {
+            expandCenter(s, i, i);
+            expandCenter(s, i, i+1);
+        }
+    }
+
+    private int expandCenter(String s, int start, int end) {
         
     }
 }
