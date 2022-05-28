84d49b7ade9d9dc6ee8f1117030306e1adb13e5e
Yonghee <79752787+Yonghee9106@users.noreply.github.com>
2022-05-28 23:23:30 +1000

    Update 0005_Longest_Palindromic_Substring.java

--- Java/0005_Longest_Palindromic_Substring.java
+++ Java/0005_Longest_Palindromic_Substring.java
@@ -1,27 +1,33 @@
 class Solution {
     public String longestPalindrome(String s) {
         int startIndex = 0;
-        int endIndex = 0;
+        int lenOfPalindrome = 0;
         int len = s.length();
 
         if(len < 2) {
             return s;
         }
 
+        // from all elements in s, expand mirrored area to find the longest palindrome
         for(int i=0; i<len; i++) {
-            int len1 = expandCenter(s, i, i);
-            int len2 = expandCenter(s, i, i+1);
+            // get length of palindrome
+            int len1 = expandCenter(s, i, i);       // for odd palindrome
+            int len2 = expandCenter(s, i, i+1);     // for even palindrome
 
-            if(endIndex < Math.max(len1, len2)) {
+            // if current palindrome is bigger than previous one
+            if(lenOfPalindrome < Math.max(len1, len2)) {
+                // i is the middle index of palindrome, find start index and total length of palindrome
                 startIndex = (len1>len2) ? (i-len1/2) : (i-len2/2+1);
-                endIndex = Math.max(len1, len2);
+                lenOfPalindrome = Math.max(len1, len2);
             }
         }
-        return s.substring(startIndex, endIndex+startIndex);
+        return s.substring(startIndex, startIndex + lenOfPalindrome);
     }
 
+    // from start and end elements, expand mirrored area
     private int expandCenter(String s, int start, int end) {
         while(start >= 0 && end < s.length()) {
+            // if it is mirrored, start goes left and, end goes right side of string
             if(s.charAt(start) == s.charAt(end)) {
                 start--;
                 end++;
@@ -30,6 +36,8 @@ class Solution {
                 break;
             }
         }
-        return end - start - 2 + 1;
+        // return the length of palindrome
+        // end - start + 1 = total length, - 2 means current expanded elements are not mirrored
+        return end - start + 1 - 2;
     }
 }
