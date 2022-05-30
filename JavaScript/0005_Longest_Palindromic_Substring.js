c817c304793d2fd699dbb81c6210e5ffdb997b25
Yonghee <79752787+Yonghee9106@users.noreply.github.com>
2022-05-30 11:06:20 +1000

    Update 0005_Longest_Palindromic_Substring.js

--- JavaScript/0005_Longest_Palindromic_Substring.js
+++ JavaScript/0005_Longest_Palindromic_Substring.js
@@ -2,6 +2,24 @@
  * @param {string} s
  * @return {string}
  */
- var longestPalindrome = function(s) {
-    
+var longestPalindrome = function(s) {
+    let startIndex = 0;
+    let endIndex = 0;
+
+    for (let center1 = 0; center1 < s.length; center1++) {
+
+      for (let center2 of [center1, center1+1]) {
+
+        let l = center1;
+        let r = center2;
+        while(s[l] && s[l] === s[r]) {
+            [startIndex, endIndex] = (r-l+1) > (endIndex-startIndex+1) ? [l, r] : [startIndex, endIndex];
+            l--, r++;
+        }
+
+      }
+
+    }
+
+    return s.substring(startIndex, endIndex+1);
 };
