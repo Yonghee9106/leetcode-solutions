c5b20a0721851f10b5f1b7e028a82fb31d9feb23
Yonghee <79752787+Yonghee9106@users.noreply.github.com>
2022-06-04 19:37:00 +1000

    Update 0006_Zigzag_Conversion.java

--- Java/0006_Zigzag_Conversion.java
+++ Java/0006_Zigzag_Conversion.java
@@ -1,6 +1,12 @@
 class Solution {
     public String convert(String s, int numRows) {
         // if the numRows is 1, no zigzag pattern
-        if (numRows == 1) return s;
-
+        if (numRows == 1) {
+            return s;
+        }
+        
+        StringBuilder zigzag = new StringBuilder();
+        int len = s.length();
+        int cycle = 2 * numRows - 2;
+    }
 }
