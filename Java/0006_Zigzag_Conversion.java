94596eb0d520af2473738169dc944e420b4cba35
Yonghee <79752787+Yonghee9106@users.noreply.github.com>
2022-06-05 09:34:36 +1000

    Update 0006_Zigzag_Conversion.java

--- Java/0006_Zigzag_Conversion.java
+++ Java/0006_Zigzag_Conversion.java
@@ -8,5 +8,16 @@ class Solution {
         StringBuilder zigzag = new StringBuilder();
         int len = s.length();
         int cycle = 2 * numRows - 2;
+
+        for (int row = 0; row < numRows; row++) {
+            for (int i = 0; i + row < len; i += cycle) {
+                zigzag.append(s.charAt(i + row));
+                if (row != 0 && row != numRows - 1 && i + cycle - row < len) {
+                    zigzag.append(s.charAt(i + cycle - row));
+                }
+            }
+        }
+        
+        return zigzag.toString();
     }
 }
