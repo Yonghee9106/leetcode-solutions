c399a51f78a1b9fbe715a6b27297fd85c862270b
Yonghee <79752787+Yonghee9106@users.noreply.github.com>
2022-06-05 10:27:03 +1000

    Update 0006_Zigzag_Conversion.java

--- Java/0006_Zigzag_Conversion.java
+++ Java/0006_Zigzag_Conversion.java
@@ -7,14 +7,16 @@ class Solution {
         
         StringBuilder zigzag = new StringBuilder();
         int len = s.length();
-        int cycle = 2 * numRows - 2;                // zigzag cycle formula
+        // zigzag cycle formula
+        // after this cycle, same pattern comes again
+        int cycle = 2 * numRows - 2;
 
         // from row 0, visit all rows and append characters
         for (int row = 0; row < numRows; row++) {
             // cycleIdx is a cycle index (e.g 1st cycle, 2nd cycle...)
             for (int cycleIdx = 0; cycleIdx + row < len; cycleIdx += cycle) {
                 zigzag.append(s.charAt(cycleIdx + row));
-                // except the first and last row, each row has one another character between cycle 
+                // except the first and last rows, each row has one another character between cycle 
                 if (row != 0 && row != numRows - 1 && cycleIdx + cycle - row < len) {
                     zigzag.append(s.charAt(cycleIdx + cycle - row));
                 }
