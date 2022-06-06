8deb672ad133c117ebf83fb06d29831c401cebc5
Yonghee <79752787+Yonghee9106@users.noreply.github.com>
2022-06-06 10:44:09 +1000

    Update 0006_Zigzag_Conversion.js

--- JavaScript/0006_Zigzag_Conversion.js
+++ JavaScript/0006_Zigzag_Conversion.js
@@ -4,5 +4,23 @@
  * @return {string}
  */
 var convert = function(s, numRows) {
+    const len = s.length;
+
+    if (numRows < 2 || len < numRows) {
+        return s;
+    }
+
+    const rows = new Array(numRows).fill('');
+    let reverse = false;
+    let count = 0;
+
+    for (let i = 0; i < len; i++) {
+        rows[count] += s[i];
+        reverse? count--: count++;
+        if (count === numRows - 1 || count === 0) {
+            reverse = !reverse;
+        }
+    }
     
+    return rows.join('');
 };
