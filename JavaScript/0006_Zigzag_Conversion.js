8de76b64a6e92915d2c27e0a976b3cd4ff520173
Yonghee <79752787+Yonghee9106@users.noreply.github.com>
2022-06-06 11:06:04 +1000

    Update 0006_Zigzag_Conversion.js

--- JavaScript/0006_Zigzag_Conversion.js
+++ JavaScript/0006_Zigzag_Conversion.js
@@ -17,13 +17,19 @@ var convert = function(s, numRows) {
     let reverse = false;
     let count = 0;
 
+    // save characters in the correct rows, find the correct rows by count
+    // if reach the end or start of the rows, change reverse value
+    // rows[0] = A       E
+    // rows[1] =   B   D   F
+    // rows[2] =     C
     for (let i = 0; i < len; i++) {
-        rows[count] += s[i];
-        reverse? count--: count++;
-        if (count === numRows - 1 || count === 0) {
-            reverse = !reverse;
+        rows[count] += s[i];                            // save characters in the correct rows
+        reverse? count--: count++;                      // go down or up to the next rows
+        if (count === numRows - 1 || count === 0) {     // if reach the end or start of the rows,
+            reverse = !reverse;                         // change reverse value
         }
     }
     
+    // rows.join('') = AEBDFC
     return rows.join('');
 };
