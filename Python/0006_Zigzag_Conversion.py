67a6fb744282176e0d0047da8038a10352237e4e
Yonghee <79752787+Yonghee9106@users.noreply.github.com>
2022-06-07 14:44:08 +1000

    Update 0006_Zigzag_Conversion.py

--- Python/0006_Zigzag_Conversion.py
+++ Python/0006_Zigzag_Conversion.py
@@ -8,14 +8,20 @@ class Solution:
         rows = [''] * numRows           # create an array of length numRows and fill with ''
         currRow, direction = 0, 1
 
-        for char in s:
-            rows[currRow] += char
-            if currRow == 0:
-                direction = 1
-            elif currRow == numRows -1:
-                direction = -1
-            currRow += direction
+        # save characters in the correct rows, find the correct rows by currRow
+        # if reach the end or start of the rows, change direction value
+        # rows[0] = A       E
+        # rows[1] =   B   D   F
+        # rows[2] =     C
+        for char in s:                      # for all characters in input s
+            rows[currRow] += char           # add character to current Row
+            if currRow == 0:                # if not reached the end of rows,
+                direction = 1               # keep going down
+            elif currRow == numRows -1:     # if reached the end of rows,
+                direction = -1              # change direction, and go up
+            currRow += direction            # positive direction = go down, negative direction = go up
 
+        # ''.join(rows) = AEBDFC
         return ''.join(rows)
 
 print(Solution().convert("PAYPALISHIRING", 3))              # Example 1
