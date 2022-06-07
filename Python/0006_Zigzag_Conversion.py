58dc677e2bcf0575c982b4a0897f192e05ace11a
Yonghee <79752787+Yonghee9106@users.noreply.github.com>
2022-06-07 14:23:34 +1000

    Update 0006_Zigzag_Conversion.py

--- Python/0006_Zigzag_Conversion.py
+++ Python/0006_Zigzag_Conversion.py
@@ -1,3 +1,17 @@
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
-        
+        if numRows < 2 or numRows >= len(s):
+            return s
+
+        rows = [''] * numRows
+        rowNum, direction = 0, 1
+
+        for char in s:
+            rows[rowNum] += char
+            if rowNum == 0:
+                direction = 1
+            elif rowNum == numRows -1:
+                direction = -1
+            rowNum += direction
+
+        return ''.join(rows)
