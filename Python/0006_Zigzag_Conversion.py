37a173b2d425af83ae8b272ea75bc52448cf631f
Yonghee <79752787+Yonghee9106@users.noreply.github.com>
2022-06-07 14:38:10 +1000

    Update 0006_Zigzag_Conversion.py

--- Python/0006_Zigzag_Conversion.py
+++ Python/0006_Zigzag_Conversion.py
@@ -1,18 +1,20 @@
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
+
+        # no zigzag pattern for these cases
         if numRows < 2 or numRows >= len(s):
             return s
 
-        rows = [''] * numRows
-        rowNum, direction = 0, 1
+        rows = [''] * numRows           # create an array of length numRows and fill with ''
+        currRow, direction = 0, 1
 
         for char in s:
-            rows[rowNum] += char
-            if rowNum == 0:
+            rows[currRow] += char
+            if currRow == 0:
                 direction = 1
-            elif rowNum == numRows -1:
+            elif currRow == numRows -1:
                 direction = -1
-            rowNum += direction
+            currRow += direction
 
         return ''.join(rows)
 
