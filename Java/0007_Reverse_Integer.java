de8d17ece348f6910ee807269d2986ecff8d126b
Yonghee <79752787+Yonghee9106@users.noreply.github.com>
2022-06-09 11:19:43 +1000

    Update 0007_Reverse_Integer.java

--- Java/0007_Reverse_Integer.java
+++ Java/0007_Reverse_Integer.java
@@ -1,5 +1,16 @@
 class Solution {
     public int reverse(int x) {
-        
+        int reverse = 0;
+
+        while (x != 0) {
+            int tail = x % 10;
+            int tempReverse = reverse * 10 + tail;
+            if ((tempReverse - tail) / 10 != reverse) {
+                return 0;
+            }
+            reverse = tempReverse;
+            x = x / 10;
+        }
+        return reverse;
     }
 }
