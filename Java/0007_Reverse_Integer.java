38c2daf41c2abf4793d91bb488aa5cb390e5a2e1
Yonghee <79752787+Yonghee9106@users.noreply.github.com>
2022-06-09 11:28:09 +1000

    Update 0007_Reverse_Integer.java

--- Java/0007_Reverse_Integer.java
+++ Java/0007_Reverse_Integer.java
@@ -2,14 +2,15 @@ class Solution {
     public int reverse(int x) {
         int reverse = 0;
 
+        // for all digits in x, pop last digit(tail) and push it to the back of the reverse
         while (x != 0) {
-            int tail = x % 10;
-            int tempReverse = reverse * 10 + tail;
-            if ((tempReverse - tail) / 10 != reverse) {
+            int tail = x % 10;                              // pop last digit
+            int tempReverse = reverse * 10 + tail;          // push it to the back of the reverse
+            if ((tempReverse - tail) / 10 != reverse) {     // check overflows
                 return 0;
             }
             reverse = tempReverse;
-            x = x / 10;
+            x = x / 10;                                     // remove last digit
         }
         return reverse;
     }
