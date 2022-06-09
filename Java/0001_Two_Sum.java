43d8b880a93348623a19e44988c6889e5f326e8b
Yonghee <79752787+Yonghee9106@users.noreply.github.com>
2022-06-09 23:56:47 +1000

    Update 0001_Two_Sum.java

--- Java/0001_Two_Sum.java
+++ Java/0001_Two_Sum.java
@@ -1,21 +1,33 @@
-class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        int[] result = new int[2];                                      // instantiate new array for result
-        int diff;
+// import java.util.Arrays;
+// import java.util.HashMap;
+// import java.util.Map;
 
-        Map<Integer, Integer> map = new HashMap<Integer, Integer>();    // create hashmap (key: nums element, value: index)
+// class LeetCode {
 
-        for(int i=0; i<nums.length; i++) {
-            diff = target - nums[i];
+// 	public static void main(String[] args) {
+// 		int[] nums = {3, 3};
+// 		int[] result = twoSum(nums, 6);
+		
+// 		System.out.println(Arrays.toString(result));
+// 	}
 
-            if(map.containsKey(diff)) {                                 // if there is a diff in map
-                result[0] = map.get(diff);
-                result[1] = i;
+	public static int[] twoSum(int[] nums, int target) {
+	    int[] result = new int[2];                                      // instantiate new array for result
+	    int diff;
 
-                return result;                                          // return diff's index and current index
-            }
-            map.put(nums[i], i);                                        // if not existing, add nums[i] as a key and index as a value
-        }
-        return result;
-    }
-}
+	    Map<Integer, Integer> map = new HashMap<Integer, Integer>();    // create hashmap (key: nums element, value: index)
+
+	    for(int i=0; i<nums.length; i++) {
+	        diff = target - nums[i];
+
+	        if(map.containsKey(diff)) {                                 // if there is a diff in map
+	            result[0] = map.get(diff);
+	            result[1] = i;
+
+	            return result;                                          // return diff's index and current index
+	        }
+	        map.put(nums[i], i);                                        // if not existing, add nums[i] as a key and index as a value
+	    }
+	    return result;
+	}
+// }
