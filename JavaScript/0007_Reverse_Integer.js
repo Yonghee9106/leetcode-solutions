27eada4fa49e8ab6853a18ad992eadf7f091f417
Yonghee <79752787+Yonghee9106@users.noreply.github.com>
2022-06-10 14:24:28 +1000

    Update 0007_Reverse_Integer.js

--- JavaScript/0007_Reverse_Integer.js
+++ JavaScript/0007_Reverse_Integer.js
@@ -3,11 +3,19 @@
  * @return {number}
  */
 var reverse = function(x) {
-    const absReversed = Math.abs(x).toString().split('').reverse().join('');
+    // x = -123
+    // 1) get absolute value of x,  x = 123
+    // 2) convert x to string,      x = '123'
+    // 3) split x,                  x = [1,2,3]
+    // 4) revese an array x,        x = [3,2,1]
+    // 5) concatenate all elements, x = '321'
+    const reversed = Math.abs(x).toString().split('').reverse().join('');
 
-    if (absReversed > 2**31) {
+    // check overflows
+    if (reversed > 2**31) {
         return 0;
     }
 
-    return absReversed * Math.sign(x);
+    // return with sign
+    return reversed * Math.sign(x);
 };
