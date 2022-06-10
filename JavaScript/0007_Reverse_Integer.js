8790894daaccb73b183eec0a1c97292de405894b
Yonghee <79752787+Yonghee9106@users.noreply.github.com>
2022-06-10 14:15:28 +1000

    Update 0007_Reverse_Integer.js

--- JavaScript/0007_Reverse_Integer.js
+++ JavaScript/0007_Reverse_Integer.js
@@ -3,5 +3,11 @@
  * @return {number}
  */
 var reverse = function(x) {
-    
+    const absReversed = Math.abs(x).toString().split('').reverse().join('');
+
+    if (absReversed > 2**31) {
+        return 0;
+    }
+
+    return absReversed * Math.sign(x);
 };
