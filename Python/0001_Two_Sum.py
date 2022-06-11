56a166b9f8bbcc5a5b68141cfeb8f925816c6e3b
Yonghee <79752787+Yonghee9106@users.noreply.github.com>
2022-06-11 10:11:08 +1000

    Update 0001_Two_Sum.py

--- Python/0001_Two_Sum.py
+++ Python/0001_Two_Sum.py
@@ -12,4 +12,8 @@ class Solution:
             
         return print("No two sum solution!")
 
-print(Solution().twoSum([2, 7, 11, 15], 9))
+    
+    
+print(Solution().twoSum([2, 7, 11, 15], 9))     # Example 1, output: [0, 1] 
+print(Solution().twoSum([3, 2, 4], 6))          # Example 2, output: [1, 2]
+print(Solution().twoSum([3, 3], 6))             # Example 3, output: [0, 1]
