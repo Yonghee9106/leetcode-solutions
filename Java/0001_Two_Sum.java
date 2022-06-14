888b3daa02520903560c2f430141f7232c7798d7
Yonghee <79752787+Yonghee9106@users.noreply.github.com>
2022-06-15 00:12:17 +1000

    Update 0001_Two_Sum.java

--- Java/0001_Two_Sum.java
+++ Java/0001_Two_Sum.java
@@ -9,16 +9,16 @@ class Solution {
 
 	    Map<Integer, Integer> diff_Map = new HashMap<Integer, Integer>();   // create hashmap (key: 'nums' element, value: index)
 
-	    for(int i=0; i<nums.length; i++) {					// for all elements in the 'nums' array
+	    for(int i=0; i<nums.length; i++) {									// for all elements in the 'nums' array
 	        diff = target - nums[i];
 
 	        if(diff_Map.containsKey(diff)) {                                // if the 'diff' exists in the 'map'
-	            result[0] = diff_Map.get(diff);				// result[0] = diff's index
-	            result[1] = i;						// result[1] = current index
+	            result[0] = diff_Map.get(diff);								// result[0] = diff's index
+	            result[1] = i;												// result[1] = current index
 
 	            return result;                                          	// diff's index and current index are two sum combination
 	        }
-	        diff_Map.put(nums[i], i);					// if not existing, add nums[i] as a key and index as a value
+	        diff_Map.put(nums[i], i);										// if the 'diff' deosn't exist, add nums[i] as a key and index as a value
 	    }
 	    return result;
 	}
@@ -36,8 +36,8 @@ class Solution {
 //		int[] nums3 = {3, 3};
 //		int[] example3 = twoSum(nums3, 6);
 //		
-//		System.out.println(Arrays.toString(example1));			// [0, 1]
-//		System.out.println(Arrays.toString(example2));			// [1, 2]
-//		System.out.println(Arrays.toString(example3));			// [0, 1]
+//		System.out.println(Arrays.toString(example1));					// [0, 1]
+//		System.out.println(Arrays.toString(example2));					// [1, 2]
+//		System.out.println(Arrays.toString(example3));					// [0, 1]
 //	}
 }
