ffeb81d31e99ff3e7387f55489ab0f291832dc0d
Yonghee <79752787+Yonghee9106@users.noreply.github.com>
2022-07-05 23:50:19 +0900

    Update 0001_Two_Sum.py

--- Python/0001_Two_Sum.py
+++ Python/0001_Two_Sum.py
@@ -14,6 +14,6 @@ class Solution:
 
     
     
-print(Solution().twoSum([2, 7, 11, 15], 9))     # Example 1, output: [0, 1] 
+print(Solution().twoSum([2, 7, 11, 15], 9))     # [0, 1] 
 print(Solution().twoSum([3, 2, 4], 6))          # [1, 2]
 print(Solution().twoSum([3, 3], 6))             # [0, 1]
